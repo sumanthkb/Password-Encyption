3191765ee3aad17ba95890547f3e93a151698e30
sumanthkb <sumanthkb222@gmail.com>
2018-05-28 17:16:32 +0530

    sxs

---
+++ Password_Encryption/Password_Encryption/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Password_Encryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Encrypt = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Decrypt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(269, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Encrypt
+            // 
+            this.Encrypt.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Encrypt.Location = new System.Drawing.Point(493, 56);
+            this.Encrypt.Name = "Encrypt";
+            this.Encrypt.Size = new System.Drawing.Size(75, 23);
+            this.Encrypt.TabIndex = 2;
+            this.Encrypt.Text = "Encrypt";
+            this.Encrypt.UseVisualStyleBackColor = false;
+            this.Encrypt.Click += new System.EventHandler(this.Encrypt_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(137, 131);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(269, 96);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Decrypt
+            // 
+            this.Decrypt.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Decrypt.Location = new System.Drawing.Point(493, 204);
+            this.Decrypt.Name = "Decrypt";
+            this.Decrypt.Size = new System.Drawing.Size(75, 23);
+            this.Decrypt.TabIndex = 4;
+            this.Decrypt.Text = "Decrypt";
+            this.Decrypt.UseVisualStyleBackColor = false;
+            this.Decrypt.Visible = false;
+            this.Decrypt.Click += new System.EventHandler(this.Decrypt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Text To Encrypt :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Cipher Text :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(645, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Decrypt);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Encrypt);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Password_Encryption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Encrypt;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Decrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Password_Encryption/Password_Encryption/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_Encryption
+{
+    public partial class Form1 : Form
+    {
+        public const string initVector = "tu89geji340t89u2";
+        public const int keysize = 256; // This constant is used to determine the keysize of the encryption algorithm.
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Encrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(textBox1.Text))
+                {
+                    //Here key is of 128 bit  
+                    //Key should be either of 128 bit or of 192 bit  
+                    richTextBox1.Text = EncryptText(textBox1.Text, "sblw-2hn4-sqoy19");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error In Encrypt Button Click" + ex.Message);
+            }
+        }
+
+        private void Decrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (richTextBox1.Text != string.Empty)
+                {
+                    //Key shpuld be same for encryption and decryption  
+                    MessageBox.Show("Plain Text :- " + DecryptText(richTextBox1.Text, "sblw-2hn4-sqoy19"));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error In Decrypt Button Click" + ex.Message);
+            }
+        }
+
+        public static string EncryptText(string input, string key)
+        {
+            try
+            {
+                string passPhrase = key;/////encryption Key text
+                byte[] initVectorBytes = Encoding.UTF8.GetBytes(initVector);
+                byte[] plainTextBytes = Encoding.UTF8.GetBytes(input);
+                PasswordDeriveBytes password = new PasswordDeriveBytes(passPhrase, null);
+                byte[] keyBytes = password.GetBytes(keysize / 8);
+                RijndaelManaged symmetricKey = new RijndaelManaged();
+                symmetricKey.Mode = CipherMode.CBC;
+                ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes, initVectorBytes);////To encrypt
+                MemoryStream memoryStream = new MemoryStream();
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                cryptoStream.FlushFinalBlock();
+                byte[] cipherTextBytes = memoryStream.ToArray();
+                memoryStream.Close();
+                cryptoStream.Close();
+                return Convert.ToBase64String(cipherTextBytes);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error In Encrypt" + ex.Message);
+                return null;
+            }
+        }
+
+        public static string DecryptText(string input, string key)
+        {
+            try
+            {
+                string passPhrase = key;/////encryption Key text same 
+                byte[] initVectorBytes = Encoding.ASCII.GetBytes(initVector);
+                byte[] cipherTextBytes = Convert.FromBase64String(input);
+                PasswordDeriveBytes password = new PasswordDeriveBytes(passPhrase, null);
+                byte[] keyBytes = password.GetBytes(keysize / 8);
+                RijndaelManaged symmetricKey = new RijndaelManaged();
+                symmetricKey.Mode = CipherMode.CBC;
+                ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, initVectorBytes);
+                MemoryStream memoryStream = new MemoryStream(cipherTextBytes);
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+                byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+                int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                memoryStream.Close();
+                cryptoStream.Close();
+                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error In Decrypt" + ex.Message);
+                return null;
+            }
+        }
+
+    }
+}
